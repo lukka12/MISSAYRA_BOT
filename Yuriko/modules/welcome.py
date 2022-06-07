f9d85a5817918f4b1548fa2e1cf0fee6dfeeacf6
SHEKHAR UPADHYAY <96115514+BlackVirusOfficial@users.noreply.github.com>
2022-06-07 22:56:54 +0530

    Update welcome.py

--- Yuriko/modules/welcome.py
+++ Yuriko/modules/welcome.py
@@ -1267,7 +1267,7 @@ __help__ = """
 
 ✗ /welcomehelp - `view more formatting information for custom welcome/goodbye messages.`
 
-*✗ Pᴏᴡᴇʀᴇᴅ 💕 Bʏ: BᴏᴛDᴜɴɪʏᴀ!*
+*✗ Pᴏᴡᴇʀᴇᴅ 💕 Bʏ: HᴅFᴀᴍᴀɪʟʏ!*
 """
 
 NEW_MEM_HANDLER = MessageHandler(
