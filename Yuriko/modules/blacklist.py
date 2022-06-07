e57f2f2ca29959aea01784e238fbffd70d9cf598
SHEKHAR UPADHYAY <96115514+BlackVirusOfficial@users.noreply.github.com>
2022-06-07 23:04:07 +0530

    Update blacklist.py

--- Yuriko/modules/blacklist.py
+++ Yuriko/modules/blacklist.py
@@ -487,7 +487,7 @@ Note:
 
 ✗ *<sticker link> can be* `https://t.me/addstickers/<sticker>` *or just* `<sticker>` *or reply to the sticker message*
 
-*✗ Pᴏᴡᴇʀᴇᴅ 💕 Bʏ: BᴏᴛDᴜɴɪʏᴀ!*
+*✗ Pᴏᴡᴇʀᴇᴅ 💕 Bʏ: HᴅFᴀᴍᴀɪʟʏ!*
 """
 BLACKLIST_HANDLER = DisableAbleCommandHandler(
     "blacklist",
