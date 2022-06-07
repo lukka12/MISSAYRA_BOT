5a959c960906ad996422ef3cd3d6201cd51441f8
SHEKHAR UPADHYAY <96115514+BlackVirusOfficial@users.noreply.github.com>
2022-06-07 23:06:42 +0530

    Update connection.py

--- Yuriko/modules/connection.py
+++ Yuriko/modules/connection.py
@@ -418,7 +418,7 @@ __help__ = """
 
 ✗ /allowconnect - `<yes/no> allow a user to connect to a chat`
 
-*✗ Pᴏᴡᴇʀᴇᴅ 💕 Bʏ: BᴏᴛDᴜɴɪʏᴀ!*
+*✗ Pᴏᴡᴇʀᴇᴅ 💕 Bʏ: HᴅFᴀᴍᴀɪʟʏ!*
 """
 
 CONNECT_CHAT_HANDLER = CommandHandler(
