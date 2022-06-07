ba99908a76cf79e55e48f96cb1826f9f12bf9397
SHEKHAR UPADHYAY <96115514+BlackVirusOfficial@users.noreply.github.com>
2022-06-07 21:49:32 +0530

    Update chatbot.py

--- Yuriko/modules/chatbot.py
+++ Yuriko/modules/chatbot.py
@@ -40,7 +40,7 @@ def kukirm(update: Update, context: CallbackContext) -> str:
             )
         else:
             update.effective_message.edit_text(
-                "Yuriko Robot Chatbot Disabled By {}.".format(mention_html(user.id, user.first_name)),
+                "MissAyra Robot Chatbot Disabled By {}.".format(mention_html(user.id, user.first_name)),
                 parse_mode=ParseMode.HTML,
             )
 
@@ -66,7 +66,7 @@ def kukiadd(update: Update, context: CallbackContext) -> str:
             )
         else:
             update.effective_message.edit_text(
-                "Yuriko Robot Chatbot Enabled By {}.".format(mention_html(user.id, user.first_name)),
+                "MissAyra Robot Chatbot Enabled By {}.".format(mention_html(user.id, user.first_name)),
                 parse_mode=ParseMode.HTML,
             )
 
@@ -139,7 +139,7 @@ def list_all_chats(update: Update, context: CallbackContext):
     update.effective_message.reply_text(text, parse_mode="HTML")
 
 __help__ = """
-✗ `Chatbot utilizes the` *YURIKO* `api which allows Yuriko to talk and provide a more interactive group chat experience.`
+✗ `Chatbot utilizes the` *MissAyra* `api which allows Yuriko to talk and provide a more interactive group chat experience.`
 
 *Admins only Commands*:
  
